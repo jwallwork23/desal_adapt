0db6c3d4459873fabeca432db74390a650a2d74b
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2021-10-25 09:49:35 -0400

    point_discharge2d: drop unused plots

--- test_cases/point_discharge2d/plot_convergence.py
+++ test_cases/point_discharge2d/plot_convergence.py
@@ -64,9 +64,9 @@ axes.set_xlabel('DoF count')
 axes.set_ylabel('Quantity of interest')
 axes.set_xticks([1.0e+03, 1.0e+04, 1.0e+05, 1.0e+06])
 if config == 'aligned':
-    axes.set_ylim([0.0, 0.2])
+    axes.set_ylim([0.05, 0.2])
 else:
-    axes.set_ylim([0.0, 0.15])
+    axes.set_ylim([0.0, 0.1])
 axes.grid(True)
 plt.tight_layout()
 plt.savefig(os.path.join(plot_dir, f'dofs_vs_qoi_{config}.jpg'), dpi=dpi)
@@ -88,7 +88,7 @@ for data, label, marker in zip(runs, labels, markers):
 axes.set_xlabel('DoF count')
 axes.set_ylabel(r'Relative QoI error (\%)')
 axes.set_xticks([1.0e+03, 1.0e+04, 1.0e+05])
-axes.set_yticks([0.01, 0.1, 1, 10, 100])
+axes.set_yticks([0.1, 1, 10, 100])
 axes.grid(True)
 plt.tight_layout()
 plt.savefig(os.path.join(plot_dir, f'dofs_vs_qoi_error_{config}.jpg'), dpi=dpi)
@@ -112,55 +112,3 @@ fig2.canvas.draw()
 axes2.set_axis_off()
 bbox = legend.get_window_extent().transformed(fig2.dpi_scale_trans.inverted())
 plt.savefig(os.path.join(plot_dir, 'legend.jpg'), bbox_inches=bbox, dpi=dpi)
-
-# Plot QoI error convergence vs wallclock
-fig, axes = plt.subplots()
-axes.loglog(uniform['wallclock'][:-1], uniform['error'], '--', marker='x', label='Uniform')
-for i, (data, label, marker) in enumerate(zip(runs, labels, markers)):
-    axes.loglog(data['wallclock'], data['error'], '--', marker=marker, label=label)
-    for wc, err, it in zip(data['wallclock'], data['error'], data['iterations']):
-        axes.annotate(it, (wc, err), color=f'C{i+1}')
-axes.set_xlabel(r'CPU time [$\mathrm s$]')
-axes.set_ylabel(r'Relative QoI error (\%)')
-axes.set_xticks([1.0e+00, 1.0e+01, 1.0e+02])
-axes.set_yticks([0.01, 0.1, 1, 10, 100])
-axes.grid(True)
-plt.tight_layout()
-plt.savefig(os.path.join(plot_dir, f'time_vs_qoi_error_{config}.jpg'))
-
-# Plot DoF count vs wallclock
-fig, axes = plt.subplots()
-axes.loglog(uniform['dofs'][:-1], uniform['wallclock'][:-1], '--', marker='x', label='Uniform')
-for i, (data, label, marker) in enumerate(zip(runs, labels, markers)):
-    axes.loglog(data['dofs'], data['wallclock'], '--', marker=marker, label=label)
-    for dofs, wc, it in zip(data['dofs'], data['wallclock'], data['iterations']):
-        axes.annotate(it, (dofs, wc), color=f'C{i+1}')
-axes.set_xlabel('DoF count')
-axes.set_ylabel(r'CPU time [$\mathrm s$]')
-axes.set_xticks([1.0e+03, 1.0e+04, 1.0e+05, 1.0e+06])
-axes.set_yticks([1.0e+00, 1.0e+01, 1.0e+02, 1.0e+03])
-axes.grid(True)
-plt.tight_layout()
-plt.savefig(os.path.join(plot_dir, f'dofs_vs_time_{config}.jpg'))
-
-# Plot QoI error convergence vs metric wallclock
-fig, axes = plt.subplots()
-for i, (data, label, marker) in enumerate(zip(runs, labels, markers)):
-    axes.loglog(data['wallclock_metric'], data['error'], '--', marker=marker, label=label, color=f'C{i+1}')
-    for dofs, wc, it in zip(data['wallclock_metric'], data['error'], data['iterations']):
-        axes.annotate(it, (dofs, wc), color=f'C{i+1}')
-axes.set_xlabel(r'metric CPU time [$\mathrm s$]')
-axes.set_ylabel(r'Relative QoI error (\%)')
-axes.grid(True)
-plt.tight_layout()
-plt.savefig(os.path.join(plot_dir, f'metric_time_vs_qoi_error_{config}.jpg'))
-
-# Plot QoI error convergence vs metric wallclock
-fig, axes = plt.subplots()
-for i, (data, label, marker) in enumerate(zip(runs, labels, markers)):
-    axes.loglog(data['wallclock_metric']/data['iterations'], data['error'], '--', marker=marker, label=label, color=f'C{i+1}')
-axes.set_xlabel(r'metric CPU time [$\mathrm s$]')
-axes.set_ylabel(r'Relative QoI error (\%)')
-axes.grid(True)
-plt.tight_layout()
-plt.savefig(os.path.join(plot_dir, f'metric_time_per_iteration_vs_qoi_error_{config}.jpg'))
