3620d3c8c7fd1ce6ed82c5c9c2dcf560e402e296
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2021-10-29 12:27:29 -0400

    point_discharge3d: jpg->pdf

--- test_cases/point_discharge3d/plot_convergence.py
+++ test_cases/point_discharge3d/plot_convergence.py
@@ -28,7 +28,6 @@ assert config in ['aligned', 'offset']
 family = parsed_args.family
 assert family in ['cg', 'dg']
 plot_dir = create_directory(os.path.join('plots', config, f'{family}1'))
-dpi = 500
 
 # Load data
 root_dir = os.path.join('outputs', config)
@@ -58,7 +57,7 @@ axes.set_ylabel('Quantity of interest')
 axes.set_xticks([1.0e+03, 1.0e+04, 1.0e+05, 1.0e+06])
 axes.grid(True)
 plt.tight_layout()
-plt.savefig(os.path.join(plot_dir, f'dofs_vs_qoi_{config}.jpg'), dpi=dpi)
+plt.savefig(os.path.join(plot_dir, f'dofs_vs_qoi_{config}.pdf'))
 
 # Plot QoI convergence vs wallclock
 fig, axes = plt.subplots()
@@ -70,7 +69,7 @@ axes.set_xlabel(r'CPU time [$\mathrm s$]')
 axes.set_ylabel('Quantity of interest')
 axes.grid(True)
 plt.tight_layout()
-plt.savefig(os.path.join(plot_dir, f'time_vs_qoi_{config}.jpg'), dpi=dpi)
+plt.savefig(os.path.join(plot_dir, f'time_vs_qoi_{config}.pdf'))
 if uniform is None:
     print('Cannot plot errors because fixed mesh benchmarks do not exist yet.')
     sys.exit(0)
@@ -92,7 +91,7 @@ axes.set_xticks([1.0e+03, 1.0e+04, 1.0e+05, 1.0e+06])
 # axes.set_yticks([0.01, 0.1, 1, 10, 100])
 axes.grid(True)
 plt.tight_layout()
-plt.savefig(os.path.join(plot_dir, f'dofs_vs_qoi_error_{config}.jpg'), dpi=dpi)
+plt.savefig(os.path.join(plot_dir, f'dofs_vs_qoi_error_{config}.pdf'))
 fig, axes = plt.subplots()
 axes.semilogx(uniform['dofs'][:-1], uniform['error'], '--', marker='x', label='Uniform')
 for data, label, marker in zip(runs, labels, markers):
@@ -104,7 +103,7 @@ axes.set_yticks(np.linspace(0, 100, 11))
 axes.set_ylim([-1, 100])
 axes.grid(True)
 plt.tight_layout()
-plt.savefig(os.path.join(plot_dir, f'dofs_vs_qoi_error_semilogx_{config}.jpg'), dpi=dpi)
+plt.savefig(os.path.join(plot_dir, f'dofs_vs_qoi_error_semilogx_{config}.pdf'))
 
 # Plot legend
 fig2, axes2 = plt.subplots()
@@ -112,34 +111,4 @@ legend = axes2.legend(*axes.get_legend_handles_labels(), fontsize=18, frameon=Fa
 fig2.canvas.draw()
 axes2.set_axis_off()
 bbox = legend.get_window_extent().transformed(fig2.dpi_scale_trans.inverted())
-plt.savefig(os.path.join(plot_dir, 'legend.jpg'), bbox_inches=bbox, dpi=dpi)
-
-# Plot QoI error convergence vs wallclock
-fig, axes = plt.subplots()
-axes.loglog(uniform['wallclock'][:-1], uniform['error'], '--', marker='x', label='Uniform')
-for i, (data, label, marker) in enumerate(zip(runs, labels, markers)):
-    axes.loglog(data['wallclock'], data['error'], '--', marker=marker, label=label)
-    for wc, err, it in zip(data['wallclock'], data['error'], data['iterations']):
-        axes.annotate(it, (wc, err), color=f'C{i+1}')
-axes.set_xlabel(r'CPU time [$\mathrm s$]')
-axes.set_ylabel(r'Relative QoI error (\%)')
-axes.set_xticks([1.0e+00, 1.0e+01, 1.0e+02, 1.0e+03])
-# axes.set_yticks([0.01, 0.1, 1, 10, 100])
-axes.grid(True)
-plt.tight_layout()
-plt.savefig(os.path.join(plot_dir, f'time_vs_qoi_error_{config}.jpg'))
-
-# Plot DoF count vs wallclock
-fig, axes = plt.subplots()
-axes.loglog(uniform['dofs'][:-1], uniform['wallclock'][:-1], '--', marker='x', label='Uniform')
-for i, (data, label, marker) in enumerate(zip(runs, labels, markers)):
-    axes.loglog(data['dofs'], data['wallclock'], '--', marker=marker, label=label)
-    for dofs, wc, it in zip(data['dofs'], data['wallclock'], data['iterations']):
-        axes.annotate(it, (dofs, wc), color=f'C{i+1}')
-axes.set_xlabel('DoF count')
-axes.set_ylabel(r'CPU time [$\mathrm s$]')
-# axes.set_xticks([1.0e+03, 1.0e+04, 1.0e+05, 1.0e+06])
-# axes.set_yticks([1.0e+00, 1.0e+01, 1.0e+02, 1.0e+03])
-axes.grid(True)
-plt.tight_layout()
-plt.savefig(os.path.join(plot_dir, f'dofs_vs_time_{config}.jpg'))
+plt.savefig(os.path.join(plot_dir, 'legend.pdf'), bbox_inches=bbox)
