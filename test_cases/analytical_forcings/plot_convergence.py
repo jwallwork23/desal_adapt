18ec2ae02c9488091e9ba7c0e687da157e6f407c
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2021-10-29 12:28:21 -0400

    analytical_forcings: jpg->pdf

--- test_cases/analytical_forcings/plot_convergence.py
+++ test_cases/analytical_forcings/plot_convergence.py
@@ -7,20 +7,16 @@ import pandas as pd
 
 # Parse arguments
 parser = Parser(prog='test_cases/analytical_forcings/plot_convergence.py')
-parser.add_argument('configuration', 'aligned', help="""
+parser.add_argument('-configuration', 'offset', help="""
     Choose from 'aligned' and 'offset'.
     """)
 parser.add_argument('-family', 'cg')
-parser.add_argument('-dpi', 500, help="""
-    Dots per inch (default 500).
-    """)
 parsed_args = parser.parse_args()
 config = parsed_args.configuration
 assert config in ['aligned', 'offset']
 family = parsed_args.family
 space = f'{family}1'
 assert family in ['cg', 'dg']
-dpi = parsed_args.dpi
 cwd = os.path.dirname(__file__)
 
 # Load data
@@ -39,22 +35,25 @@ for approach in approaches:
         if approach == 'fixed_mesh':
             f = pd.read_csv(fname)
             for key in data[approach]:
-                data[approach][key].append(f[key])
+                data[approach][key].append(f[key][0])
         else:
             lines = open(fname, 'r').readlines()
             data[approach]['qoi'].append(float(lines[1].split('=')[-1]))
             data[approach]['wallclock'].append(float(lines[2].split('=')[-1].split()[0]))
 
-
 # Plot QoI vs DoF count
 plot_dir = create_directory(os.path.join(cwd, 'plots', config, f'{family}1'))
 fig, axes = plt.subplots()
-axes.semilogx(data['fixed_mesh']['dofs'], data['fixed_mesh']['qoi'], '--x', label='Fixed mesh')
+for approach, name, marker, colour in zip(approaches, names, markers, colours):
+    if approach != 'fixed_mesh':
+        continue
+    axes.semilogx(data[approach]['dofs'], data[approach]['qoi'],
+                  linestyle='--', marker=marker, label=name, color=colour)
 axes.set_xlabel('DoF count')
 axes.set_ylabel('Quantity of Interest')
 axes.grid(True)
 plt.tight_layout()
-plt.savefig(os.path.join(plot_dir, f'qoi_vs_dofs_{config}.jpg'), dpi=dpi)
+plt.savefig(os.path.join(plot_dir, f'qoi_vs_dofs_{config}.pdf'))
 
 # Plot QoI vs CPU time
 fig, axes = plt.subplots()
@@ -65,7 +64,7 @@ axes.set_xlabel(r'CPU time [$\mathrm s$]')
 axes.set_ylabel('Quantity of Interest')
 axes.grid(True)
 plt.tight_layout()
-plt.savefig(os.path.join(plot_dir, f'qoi_vs_time_{config}.jpg'), dpi=dpi)
+plt.savefig(os.path.join(plot_dir, f'qoi_vs_time_{config}.pdf'))
 
 # Plot QoI vs level
 fig, axes = plt.subplots()
@@ -81,7 +80,7 @@ axes.set_xticks([1.0e+06, 1.0e+07, 1.0e+08])
 axes.set_yticks([9.275e+06, 9.3e+06, 9.325e+06, 9.350e+06, 9.375e+06])
 axes.grid(True)
 plt.tight_layout()
-plt.savefig(os.path.join(plot_dir, f'qoi_vs_target_{config}.jpg'), dpi=dpi)
+plt.savefig(os.path.join(plot_dir, f'qoi_vs_target_{config}.pdf'))
 
 # Plot legend
 fig2, axes2 = plt.subplots()
@@ -89,4 +88,4 @@ legend = axes2.legend(*axes.get_legend_handles_labels(), fontsize=18, frameon=Fa
 fig2.canvas.draw()
 axes2.set_axis_off()
 bbox = legend.get_window_extent().transformed(fig2.dpi_scale_trans.inverted())
-plt.savefig(os.path.join(plot_dir, 'legend.jpg'), bbox_inches=bbox, dpi=dpi)
+plt.savefig(os.path.join(plot_dir, 'legend.pdf'), bbox_inches=bbox)
