5a7a2c389b9a517a7ad2f824dfee986831d1867a
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2021-11-01 09:52:56 -0400

    point_discharge2d: add analytical QoI values to convergence plot

--- test_cases/point_discharge2d/plot_convergence.py
+++ test_cases/point_discharge2d/plot_convergence.py
@@ -49,27 +49,10 @@ for tag, label in zip(tags, names):
     if data is not None:
         runs.append(data)
         labels.append(label)
-
-# Plot QoI convergence vs DoFs
-fig, axes = plt.subplots()
-if uniform is not None:
-    axes.semilogx(uniform['dofs'], uniform['qois'], '--', marker='x', label='Uniform')
-for data, label, marker in zip(runs, labels, markers):
-    axes.semilogx(data['dofs'], data['qois'], '--', marker=marker, label=label)
-axes.set_xlabel('DoF count')
-axes.set_ylabel('Quantity of interest')
-axes.set_xticks([1.0e+03, 1.0e+04, 1.0e+05, 1.0e+06])
-if config == 'aligned':
-    axes.set_ylim([0.05, 0.2])
-else:
-    axes.set_ylim([0.0, 0.1])
-axes.grid(True)
-plt.tight_layout()
-plt.savefig(os.path.join(plot_dir, f'dofs_vs_qoi_{config}.svg'))
-plt.savefig(os.path.join(plot_dir, f'dofs_vs_qoi_{config}.pdf'))
-if uniform is None:
-    print('Cannot plot errors because fixed mesh benchmarks do not exist yet.')
-    sys.exit(0)
+f = open(os.path.join(root_dir, 'fixed_mesh', space, 'analytical_qoi.log'), 'r')
+qoi_analytical = float(f.readlines()[-1].split()[-1])
+print(f"Analytical QoI value = {qoi_analytical}")
+f.close()
 
 # QoI errors
 truth = uniform['qois'][-1]
@@ -99,14 +82,38 @@ axes.set_ylabel(r'Relative QoI error (\%)')
 axes.set_xticks([1.0e+03, 1.0e+04, 1.0e+05, 1.0e+06])
 axes.set_yticks(np.linspace(0, 100, 11))
 axes.set_ylim([-1, 100])
+xlim = axes.get_xlim()
 axes.grid(True)
 plt.tight_layout()
 plt.savefig(os.path.join(plot_dir, f'dofs_vs_qoi_error_semilogx_{config}.svg'))
 plt.savefig(os.path.join(plot_dir, f'dofs_vs_qoi_error_semilogx_{config}.pdf'))
 
+# Plot QoI convergence vs DoFs
+fig, axes = plt.subplots()
+axes.hlines(qoi_analytical, *xlim, color='k', linestyle='-', label='Analytical')
+if uniform is not None:
+    axes.semilogx(uniform['dofs'], uniform['qois'], '--', marker='x', label='Uniform')
+for data, label, marker in zip(runs, labels, markers):
+    axes.semilogx(data['dofs'], data['qois'], '--', marker=marker, label=label)
+axes.set_xlabel('DoF count')
+axes.set_ylabel('Quantity of interest')
+axes.set_xticks([1.0e+03, 1.0e+04, 1.0e+05, 1.0e+06])
+axes.set_xlim(xlim)
+if config == 'aligned':
+    axes.set_ylim([0.05, 0.2])
+else:
+    axes.set_ylim([0.0, 0.1])
+axes.grid(True)
+plt.tight_layout()
+plt.savefig(os.path.join(plot_dir, f'dofs_vs_qoi_{config}.svg'))
+plt.savefig(os.path.join(plot_dir, f'dofs_vs_qoi_{config}.pdf'))
+
 # Plot legend
 fig2, axes2 = plt.subplots()
-legend = axes2.legend(*axes.get_legend_handles_labels(), fontsize=18, frameon=False, ncol=3)
+lines, labels = axes.get_legend_handles_labels()
+lines = [lines.pop(-1)] + lines
+labels = [labels.pop(-1)] + labels
+legend = axes2.legend(lines, labels, fontsize=18, frameon=False, ncol=3)
 fig2.canvas.draw()
 axes2.set_axis_off()
 bbox = legend.get_window_extent().transformed(fig2.dpi_scale_trans.inverted())
